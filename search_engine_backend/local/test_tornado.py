bb22acd225302da88b122ffbc3b397e1dac3513b
wyfunique <yifanwang2017@gmail.com>
2018-10-20 22:50:11 -0400

    On branch demo  Changes to be committed:     Integrated front end pages into back end

--- search_engine_backend/local/test_tornado.py
+++ search_engine_backend/local/test_tornado.py
@@ -1,10 +1,17 @@
+import os
 import tornado.ioloop
 from tornado.web import RequestHandler
 import torndb
 
+class article:
+    def __init__(self):
+        self.title = "Test"
+        self.content = "This is a test article"
+        self.publish_date = "Oct 20, 2018"
+
 class MainHandler(RequestHandler):
     def get(self):
-        self.write("Hello, world")
+        self.render("static/index.html")
 
 class InvertedIndexHandler(RequestHandler):
     def get(self, word):
@@ -20,11 +27,28 @@ class InvertedIndexHandler(RequestHandler):
                 res += doc + '\n'
             self.write(res)
 
+class ArticleListHandler(RequestHandler):
+    def get(self):
+        art = article()
+        args = {
+            "articles": [art]
+        }
+        self.render("static/blog-list.html", **args)
+
 def make_app():
-    return tornado.web.Application([
+    url_handlers = [
         (r"/", MainHandler),
-        (r"/inverted-index/(.+)", InvertedIndexHandler)
-    ])
+        (r"/inverted-index/(.+)", InvertedIndexHandler),
+        (r"/article-list", ArticleListHandler)
+    ]
+
+    settings = {
+        "static_path": os.path.join(os.path.dirname(os.path.abspath(__file__)), "static") 
+    }
+    #template_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "template")
+    #print template_path
+
+    return tornado.web.Application(url_handlers, **settings)
 
 if __name__ == "__main__":
     app = make_app()
