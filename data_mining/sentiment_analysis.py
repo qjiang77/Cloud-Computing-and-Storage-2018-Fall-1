f0b806ce3b47a272c384ccbade23811348015aef
Jy Guo <jingyang.guo@ufl.edu>
2018-11-29 16:46:20 -0500

    updated sentiment analysis : process database fetched articles

--- data_mining/sentiment_analysis.py
+++ data_mining/sentiment_analysis.py
@@ -4,19 +4,36 @@ from textblob import TextBlob
 
 basepath = '/Users/jyguo/Desktop/Courses/Cloud_Computing/articles/articles_cnbc/'
 
-def test(article_path): ### input: article's path
+def sentiment_analysis(article): ### input : article text string
+		analysis = TextBlob(article.decode('utf-8'))
+		sentiment = analysis.sentiment.polarity ### > 0 : positive; < 0 : negative; == 0 : neutral
+		polar = ''
+		if sentiment > 0:
+			polar = 'positive'
+		elif sentiment < 0:
+			polar = 'negative'
+		else:
+			polar = 'neutral'
+		return sentiment, polar
+
+def data_batch_analysis(article_list): ### input : list of articles from database
+	res = []
+	polar_res = {'positive' : 0, 'negative' : 0, 'neutral' : 0}
+	
+	for article in article_list:
+		text = article['text']
+		sentiment, polar = sentiment_analysis(text)
+		res.append(sentiment)
+		polar_res[polar] += 1
+
+	return polar_res
+
+def sentiment_analysis_from_local_file(article_path): ### input: article's path
 		f = open(article_path, 'r')
 		f.readline()
-		text = f.read().decode('utf-8')
-		analysis = TextBlob(text)
-		return analysis.sentiment.polarity ### > 0 : positive; < 0 : negative; == 0 : neutral
+		return sentiment_analysis(f.read())
 
-def sentiment_analysis(article): ### input : article string
-		article = article.decode('utf-8')
-		analysis = TextBlob(text)
-		return analysis.sentiment.polarity ### > 0 : positive; < 0 : negative; == 0 : neutral
-
-def batch_analysis(path): ### input: multiple articles' directory
+def batch_analysis_from_local_file(path): ### input: multiple articles' directory
 	paths = []
 	for name in os.listdir(path):
 		paths.append(path + name)
@@ -25,15 +42,7 @@ def batch_analysis(path): ### input: multiple articles' directory
 	polar_res = {'positive' : 0, 'negative' : 0, 'neutral' : 0}
 
 	for p in paths:
-		sentiment = test(p)
-		polar = ''
-		if sentiment > 0:
-			polar = 'positive'
-		elif sentiment < 0:
-			polar = 'negative'
-		else:
-			polar = 'neutral'
-		
+		sentiment, polar = test(p)
 		res.append(sentiment)
 		polar_res[polar] += 1
 
@@ -47,23 +56,7 @@ if __name__ == '__main__':
 			newname = newname.replace(':', '')
 			os.rename(basepath+filename, basepath+newname)
 	'''
-	print batch_analysis(basepath)
-
-
-'''
-sentiment_url = 'http://text-processing.com/api/sentiment/'
-headers = {'content-type': 'application/sentiment', 'Accept-Charset': 'UTF-8'}
-
-
-def sentiment_analysis(article_path):
-		f = open(article_path, 'r')
-		f.readline()
-		text = f.read().split('|')[-1]
-		data = {'text': text}
-		r_json = None
-		try:
-			r_json = requests.post(sentiment_url, data = data, headers = headers).json()
-		except:
-			pass
-		return (r_json['probability']['neg'], r_json['probability']['pos']) if r_json else None
-'''
+	a = {'text' : 'good'}
+	b = {'text': 'bad'}
+	print data_batch_analysis([a, b])
+	#print batch_analysis_from_local_file(basepath)
