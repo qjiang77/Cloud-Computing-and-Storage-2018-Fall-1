24825c0887e4e46af9ba7c68a7923845ede73fe1
Jy Guo <jingyang.guo@ufl.edu>
2018-12-04 21:31:49 -0500

    Added reference field

--- data_mining/mapper.py
+++ data_mining/mapper.py
@@ -9,33 +9,32 @@ class mapper:
 		self.cities = pd.read_csv(path)
 		
 	def checkCities(self, article_list):
-		whole_text = ' '.join([article['text'] for article in article_list])
-
-		words = whole_text.split(' ')
-		count = Counter(words)
-		double_words = [words[i] + ' ' + words[i + 1] for i in range(len(words) - 1)]
-		double_count = Counter(double_words)
-		
 		cities = self.cities
+		freq = [0 for _ in range(cities.shape[0])]
+		references = [[] for _ in range(cities.shape[0])]
+		for article in article_list:
+			text = article['text']
+			words = text.split(' ')
+			count = Counter(words)
+			double_words = [words[i] + ' ' + words[i + 1] for i in range(len(words) - 1)]
+			double_count = Counter(double_words)
 
-		freq = [0] * cities.shape[0]
-		
-		for index, row in cities.iterrows():
-			if row['City'] in count:
-				freq[index] += count[row['City']]
-			if row['AccentCity'] in count:
-				freq[index] += count[row['AccentCity']]
-			if row['City'] in double_count:
-				freq[index] += double_count[row['City']]
-			if row['AccentCity'] in double_count:
-				freq[index] += double_count[row['AccentCity']]
-		
+			for index, row in cities.iterrows():
+				ref = False
+				if row['City'] in count or row['AccentCity'] in count or row['City'] in double_count or row['AccentCity'] in double_count:
+					freq[index] += 1
+					references[index].append({'title': article['title'], 'url': article['url']})
+
 		cities['Frequency'] = pd.Series(freq).values
+		cities['References'] = pd.Series(references).values
 		cities = cities[(cities['Frequency'] > 0)]
+		#res = cities.to_dict('records')
+
 		return cities.to_dict('records')
 
 if __name__ == '__main__':
-	a = {'text' : 'gainesville good'}
-	b = {'text': 'tampa bad'}
+	a = {'title': 'gainesville daily', 'url': 'www.ggg.com', 'text' : 'gainesville perfect'}
+	b = {'title': 'tampa daily', 'url': 'www.ttt.com','text': 'tampa bad'}
+	c = {'title': 'orlando daily', 'url': 'www.ooo.com','text': 'gainesville fantastic'}
 	m = mapper(map_path)
-	print(m.checkCities([a,b]))
+	print(m.checkCities([a,b,c]))
